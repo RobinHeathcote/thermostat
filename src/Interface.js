6bd18e93bb96054e19bf46008a7d22e07d4729d8
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-12 17:55:16 +0100

    Adds Weather API

--- src/Interface.js
+++ src/Interface.js
@@ -11,6 +11,27 @@ $(document).ready(function() {
   $('#temperature').attr('class', thermostat.displayStatus());
 }
 
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+
+  });
+
+  // $displayWeather('London');
+
+function displayWeather(city) {
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+var token = '&appid=f4e84488ed545c8e2321624871711808';
+var units = '&units=metric';
+$.get(url + token + units, function(data) {
+$('#current-temperature').text(data.main.temp);
+
+});
+}
+
+
 $('#temp-up').on('click', function() { // event listener
   thermostat.upButton() // update model
    updateTemperature(); // update view
